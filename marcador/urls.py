b79842cc5ab29d2847f18e38004786a518bf03be
DJMosi <davimosi@gmail.com>
2015-10-05 09:51:24 +0300

    urls marcador step by step test

--- marcador/urls.py
+++ marcador/urls.py
@@ -17,13 +17,17 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('marcador.urls')),
+
     url(r'^user/(?P<username>[-\w]+)/$', 'marcador.views.bookmark_user',
         name='marcador_bookmark_user'),
+
     url(r'^$', 'marcador.views.bookmark_list', name='marcador_bookmark_list'),
 
 ]
+
+
 
 
 
